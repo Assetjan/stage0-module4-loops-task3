fdcc2b54ce335278f5a63fb747def41cb0672f50
Assetjan <asetziyabek@gmail.com>
2023-01-23 19:53:28 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -3,10 +3,8 @@ package school.mjc.stage0.loops.task3;
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
         int sum = 0;
-        int j = firstBoarder+1;
         for (int i = firstBoarder; i <= secondBoarder; i++){
-            sum = sum + j;
-            j++;
+            sum = sum + i;
         }
         System.out.println(sum);
     }
