e44c22f7b598989eff0932a9e1e04875c9edb2fa
Assetjan <asetziyabek@gmail.com>
2023-01-23 19:56:52 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
         String nine = "";
-        String nines[] = new String[lengthOfLastNumber];
+        String nines[] = new String[Math.abs(lengthOfLastNumber)];
         for (int i = 0; i < lengthOfLastNumber; i++){
             nine += "9";
             nines[i] = nine;
